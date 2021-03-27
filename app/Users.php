c0066072bc7130fe9892e7b1cd18462e7275d581
traynerZero <lozanomarianosapico@gmail.com>
2021-03-27 11:23:57 +0800

    index shop

--- app/Users.php
+++ app/Users.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
     //
+
+    protected $table = 'users';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'userid';
+
+    protected $fillable = ['firstname','lastname','email','password','user_type','status','remember_token','created_at','updated_at'];
+
 }
