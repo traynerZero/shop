c76a893f971c9d8411a8882092cb32337ce3ef65
traynerZero <lozanomarianosapico@gmail.com>
2021-03-27 19:15:18 +0800

    index cart view 1.1.5

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -28,22 +28,21 @@
                 <img style="width:10vh; height 10vh;" src="images/home/product1.jpg" class="img-fluid img-thumbnail" alt="Sheep">
               </td>
               <td>{{ $product['prod_name'] }}</td>
-              <td>{{ $product['price'] }}</td>
-              <td class="qty"><input type="text" class="form-control" id="input1" value="{{ $product['quantity'] }}"></td>
-                
-              <td>{{ $product['total'] }}</td>
+              <td>{{ number_format($product['price'],2) }}</td>
+              <td class="qty">{{ $product['quantity'] }}</td>
+              <td>{{ number_format($product['total'],2) }}</td>
               <td>
                 <a href="#" class="btn btn-danger btn-sm">
-                  <i class="fa fa-times"></i>
+                  <i class="fa fa-minus"></i>
+                </a>
+                <a href="#" class="btn btn-danger btn-sm">
+                  <i class="fa fa-plus"></i>
                 </a>
               </td>
             </tr>
             @endforeach
           </tbody>
         </table> 
-        <div class="d-flex justify-content-end">
-          <h5>Total: <span class="price text-success">89$</span></h5>
-        </div>
         
       <div class="modal-footer border-top-0 d-flex justify-content-between">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
