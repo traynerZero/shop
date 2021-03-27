0db4e7394e9902d0b08a6d9d540342ec1b632331
traynerZero <lozanomarianosapico@gmail.com>
2021-03-27 16:34:26 +0800

    index cart view 1.1.2

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -16,8 +16,9 @@ class TransactionController extends Controller
             
             $cart = session()->get("cart");
 
+            echo json_encode($cart);
             foreach($cart as $c){
-                echo json_encode($c);
+               
             }
 
         }else{
