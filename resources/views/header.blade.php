d550d0d44372cf2124cbbfdee266de12c335a4e1
traynerZero <lozanomarianosapico@gmail.com>
2021-03-28 16:28:30 +0800

    fix cart display 1

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -142,3 +142,15 @@
 
 
 	</script>
+
+	
+</style>
+	<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-content" style="width:50%;">
+		
+
+		
+    </div>
+  </div>
+</div>
